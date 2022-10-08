2c51347fe7ceb6f86c606f7379fc97c7db4c3cf1
watsumi <w.utsumi2@gmail.com>
2022-10-08 12:00:38 +0900

    create manifest

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,32 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { crx, defineManifest } from '@crxjs/vite-plugin';
+
+const manifest = defineManifest({
+  manifest_version: 3,
+  name: 'git-branch-name-generator',
+  description:
+    'An extension that generate git branch name from the GitHub issue page.',
+  version: '1.0.0',
+  icons: {
+    '16': 'icon.png',
+    '48': 'icon.png',
+    '128': 'icon.png',
+  },
+  permissions: ["activeTab"],
+  action: {
+    default_popup: 'index.html',
+  },
+  commands: {
+    _execute_action: {
+      suggested_key: {
+        default: "Ctrl+Shift+L",
+      },
+    },
+  },
+});
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  plugins: [react(), crx({ manifest })],
+});
